d8006777d680b55a539d6efd03cb70072ff9016a
DavidJN1011 <32164579+DavidJN1011@users.noreply.github.com>
2018-07-29 15:47:26 +0100

    Add files via upload

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -5,8 +5,11 @@ public class MainClass
     public static void Main()
     {
 
+        //if goAgain= "Y";
+
         int x = 0;
         do
+
         {
             //set the background colour to blue
             Console.BackgroundColor = ConsoleColor.DarkBlue;
@@ -29,52 +32,52 @@ public class MainClass
             // \t adds a tab to the line which helps formatting
 
             Console.Write("\t Tell me the length of the room : \t");
-            double a = Convert.ToDouble(Console.ReadLine());
+            double roomLength = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("\t Tell me the width of the room  : \t");
-            double b = Convert.ToDouble(Console.ReadLine());
+            double roomWidth = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("\t Tell me the height of the room : \t");
-            double c = Convert.ToDouble(Console.ReadLine());
+            double roomHeight = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("");
 
             Console.WriteLine("\t Check the instructions on the paint can and tell me");
             Console.Write("\t how many sq m of wall one litre of paint will cover : ");
-            double e = Convert.ToDouble(Console.ReadLine());
+            double paintCover = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("");
 
-            Console.Write("\t The area of the floor is\t\t: \t " + (a * b) + " sq metres");
+            Console.Write("\t The area of the floor is\t\t: \t " + (roomLength * roomWidth) + " sq metres");
 
             Console.WriteLine("");
 
-            Console.Write("\t The volume of the room is\t\t: \t " + (a * b * c) + " cubic metres");
+            Console.Write("\t The volume of the room is\t\t: \t " + (roomLength * roomWidth * roomHeight) + " cubic metres");
 
             // Surface area of all walls assumes that the facing walls are both the same size
 
-            double d = (a * c * 2) + (b * c * 2);
+            double wallArea = (roomLength * roomHeight * 2) + (roomWidth * roomHeight * 2);
 
             Console.WriteLine("");
 
 
-            Console.Write("\t The surface area of the walls is\t: \t " + d + " sq metres");
+            Console.Write("\t The surface area of the walls is\t: \t " + wallArea+ " sq metres");
 
             Console.WriteLine("\r\n\n");
 
 
             //limit final division calculation to 1 decimal places otherwise it can be much too long after the decimal point
 
-            double f = Math.Round(d / e, 1);
+            double paintNeeded = Math.Round(wallArea / paintCover, 1);
 
-            Console.Write("\t To paint all 4 walls you will need " + f + " litres of paint \r\n\n");
+            Console.Write("\t To paint all 4 walls you will need " + paintNeeded + " litres of paint \r\n\n");
 
 
             Console.Write("\t Happy Painting.......Press ENTER key to restart");
             Console.ReadLine();
 
 
-
+            
         } while (x < 1);
 
     }
